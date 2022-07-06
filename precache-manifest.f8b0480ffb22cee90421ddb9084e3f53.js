09d912aa5976b87d94f256a3132f438bcaa3270e
Yash Gupta <yashg3600@gmail.com>
2022-07-06 22:09:09 +0530

    Updates

---
+++ precache-manifest.f8b0480ffb22cee90421ddb9084e3f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a93d22a1578710f78a2",
+    "url": "/TextUtils/static/css/main.c2f450fd.chunk.css"
+  },
+  {
+    "revision": "1a93d22a1578710f78a2",
+    "url": "/TextUtils/static/js/main.1a93d22a.chunk.js"
+  },
+  {
+    "revision": "db20f86d0530543db744",
+    "url": "/TextUtils/static/js/1.db20f86d.chunk.js"
+  },
+  {
+    "revision": "3a2e0e0b763789a1abbb",
+    "url": "/TextUtils/static/js/2.3a2e0e0b.chunk.js"
+  },
+  {
+    "revision": "ae1ab5972405ba779003",
+    "url": "/TextUtils/static/js/runtime~main.ae1ab597.js"
+  },
+  {
+    "revision": "70b98db1fc726368997c93711b228886",
+    "url": "/TextUtils/index.html"
+  }
+];
